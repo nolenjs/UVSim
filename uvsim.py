5562bb6e8e37be43f65c0214eb3d4f796bf7569e
nolenjs <31349478+nolenjs@users.noreply.github.com>
2024-02-07 22:43:51 -0700

    Update uvsim.py

--- uvsim.py
+++ uvsim.py
@@ -2,7 +2,7 @@ class UVSim:
     def __init__(self, counter = 0, accumulator = 0):
         self.counter = counter
         self.accumulator = accumulator
-        with open("../program.txt", "r") as f:
+        with open("./program.txt", "r") as f:
             self.program = f.readlines()
             for p in range(0,len(self.program)):
                 self.program[p] = self.program[p].strip() #Remove any whitespace characters
